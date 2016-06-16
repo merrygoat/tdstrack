608b6d8825b7cba209abccecb824a5789cb39527
Peter <pc9836@bristol.ac.uk>
2016-06-16 23:31:31 +0100

    Begining of work on cell list.

--- tdstrack.py
+++ tdstrack.py
@@ -2,18 +2,11 @@ import numpy as np
 from glob import iglob
 from pandas import read_csv
 from scipy.spatial.distance import cdist
-from PIL import Image
 import matplotlib.pyplot as plt
 
+def slicecorrelation(data):
 
-def main():
-    data = []
     matches = []
-
-    for inputfile in iglob(
-            "/Volumes/WIN_DATA/Confocal/STED/Hard spheres/15-12-21/slice_[1-3]_raw_coords.txt"):
-        data.append(read_csv(inputfile, sep="\t", header=None).values)
-
     num_slices = len(data)
 
     plt.figure(figsize=(10, 10))
@@ -29,16 +22,38 @@ def main():
                 plt.imshow(matches[len(matches) - 1])
                 plt.savefig(str(slice1) + "_" + str(slice2) + ".png", dpi=100)
 
-    particlepositions = []
-    # Get particle positions from neighbour matrix
-    for slice1 in range(0, num_slices):
-        for particle in range(0, matches[slice].shape[0]):
+    return num_slices
 
-    # Loop through particles
-    #    for slice in range(0, num_slices):
-    #       for particle in range(0, matches[slice].shape[0]):
-    #            for secondslice in range(slice + 1, num_slices):
+def readinputfile():
+    data = []
 
+    for inputfile in iglob("slices/slice_[1-7]_raw_coords.txt"):
+        data.append(read_csv(inputfile, sep="\t", header=None).values)
+    # Once appending is done then condense python list into np array
+
+
+    return data
+
+def initialise_cell_list(data, num_slices):
+
+    max_x = 0
+
+    #sort data by x-coordinate
+    for z_slice in range(0, num_slices):
+        data = data[z_slice][np.lexsort(data[:][:, 0]), :]
+        newmax_x = np.max(data[:][:, 0])-np.min(data[:][:,0])
+        if newmax_x > max_x:
+            max_x = newmax_x
+
+    return data
+
+
+def main():
+
+    data = readinputfile()
+    num_slices = slicecorrelation(data)
+
+    data = initialise_cell_list(data, num_slices)
 
     print "Fin."
 
